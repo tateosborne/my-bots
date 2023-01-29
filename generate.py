45ff0febea56169837e20cf7b533972b5fef05b7
Tate P. Osborne <tate0301@gmail.com>
2023-01-28 22:28:56 -0500

    changed the postition of the box to be above on the ground

--- generate.py
+++ generate.py
@@ -3,10 +3,14 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
+x = 0
+y = 0
+z = 1.5
+
 length = 1
 width = 2
 height = 3
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()
