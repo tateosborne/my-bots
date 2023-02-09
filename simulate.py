07550e645c15f5a4db1a30aaa6656d4540691820
Tate P. Osborne <tate0301@gmail.com>
2023-02-09 11:56:07 -0500

    added motors to the two joints so that the robot stands on its toes

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
 import numpy
+import random
+from math import pi
 
 
 physicsClient = p.connect(p.GUI)
@@ -19,13 +21,17 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_BackLeg", controlMode=p.POSITION_CONTROL, targetPosition=-pi/4.0, maxForce=500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_FrontLeg", controlMode=p.POSITION_CONTROL, targetPosition=pi/4.0, maxForce=500)
+    
     time.sleep(1/60)
 
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
